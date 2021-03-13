f9b68caa4b28f5545cf5d6fed45d8c718cc6de6b
nizar-ing <you@example.com>
2021-03-13 22:12:32 +0100

    Destructering arguments instead of using props

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import TableBody from "./tableBody";
 import TableHeader from "./tableHeader";
-const Table = (props) => {
-  const { data, columns, sortColumn, onSort } = props;
+const Table = ({ data, columns, sortColumn, onSort }) => {
   return (
     <table
       className='table table-dark table-hover'
       style={{ borderRadius: "5px 5px 0 0" }}
     >
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
-      <TableBody data={data} columns={props.columns} />
+      <TableBody data={data} columns={columns} />
     </table>
   );
 };
