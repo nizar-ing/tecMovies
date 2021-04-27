4b288d294719818f65ae8ee2e459f6ee2d17fb0f
nizar-ing <nizar.ilahi@gmail.com>
2021-04-27 22:56:48 +0100

    some refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,19 +1,23 @@
 import http from './httpService';
 import {apiUrl} from "../config.json";
+
+function movieUrl(movieId){
+    return `${apiUrl}/movies/${movieId}`;
+}
 export function getMovies() {
     return http.get(`${apiUrl}/movies`);
 }
 export function getMovie(movieId) {
-    return http.get(`${apiUrl}/movies/${movieId}`);
+    return http.get(movieUrl(movieId));
 }
 export function saveMovie(movie) {
     if(movie._id){
         const body = {...movie};
         delete body._id;
-        return http.put(`${apiUrl}/movies/${movie._id}`, body);
+        return http.put(movieUrl(movie._id), body);
     }
     return http.post(`${apiUrl}/movies`, movie);
 }
 export function deleteMovie(movieId) {
-    return http.delete(`${apiUrl}/movies/${movieId}`);
+    return http.delete(movieUrl(movieId));
 }
